9748e614b9f7bb8b4f71f62fb6c82149e982621b
selvestro <dima@seliverstov.info>
2015-05-29 20:43:18 +0300

    stat_local edited

--- assets/stat_local.js
+++ assets/stat_local.js
@@ -1,5 +1,4 @@
 $(function () {
-  // data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type
   var tax_data = [
        {"period": "2011 Q3", "licensed": 3407, "sorned": 660},
        {"period": "2011 Q2", "licensed": 3351, "sorned": 629},
@@ -11,24 +10,6 @@ $(function () {
        {"period": "2006 Q4", "licensed": 3245, "sorned": null},
        {"period": "2005 Q4", "licensed": 3289, "sorned": null}
   ];
-  Morris.Line({
-    element: 'hero-graph',
-    data: tax_data,
-    xkey: 'period',
-    ykeys: ['licensed', 'sorned'],
-    labels: ['Licensed', 'Off the road']
-  });
-
-  Morris.Donut({
-    element: 'hero-donut',
-    data: [
-      {label: 'Jam', value: 25 },
-      {label: 'Frosted', value: 40 },
-      {label: 'Custard', value: 25 },
-      {label: 'Sugar', value: 10 }
-    ],
-    formatter: function (y) { return y + "%" }
-  });
 
   Morris.Line({
   element: 'line-example',
@@ -103,38 +84,6 @@ $(function () {
   ykeys: ['area', 'population'],
   labels: ['Series A', 'Series B']
 });
-
-  Morris.Bar({
-    element: 'hero-bar',
-    data: [
-      {device: 'iPhone', geekbench: 136},
-      {device: 'iPhone 3G', geekbench: 137},
-      {device: 'iPhone 3GS', geekbench: 275},
-      {device: 'iPhone 4', geekbench: 380},
-      {device: 'iPhone 4S', geekbench: 655},
-      {device: 'iPhone 5', geekbench: 1571}
-    ],
-    xkey: 'device',
-    ykeys: ['geekbench'],
-    labels: ['Geekbench'],
-    barRatio: 0.4,
-    xLabelAngle: 35,
-    hideHover: 'auto'
-  });
-
-  new Morris.Line({
-    element: 'examplefirst',
-    xkey: 'year',
-    ykeys: ['value'],
-    labels: ['Value'],
-    data: [
-      {year: '2008', value: 20},
-      {year: '2009', value: 10},
-      {year: '2010', value: 5},
-      {year: '2011', value: 5},
-      {year: '2012', value: 20}
-    ]
-  });
 
   $('.code-example').each(function (index, el) {
     eval($(el).text());
